438bb6cb93478fb0df9ae61837bba4fe772b7d16
abdenassar01 <abdenassaramimi99@gmail.com>
2024-04-30 17:52:04 +0100

    adding video modal

---
+++ src/components/core/landing-page/video-player.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Modal } from '@/components';
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+export function VideoPlayer() {
+  const [visibleModal, setVisibleModal] = useState<boolean>(false);
+
+  return (
+    <>
+      <button onClick={() => setVisibleModal((prev) => !prev)}>
+        <Image
+          className='w-[40vw] sm:w-[90vw]'
+          width={350}
+          height={275}
+          alt=''
+          src='/video-main.png'
+        />
+      </button>
+      <Modal
+        width={800}
+        height={450}
+        className='relative !aspect-video overflow-hidden p-0'
+        setVisible={setVisibleModal}
+        visible={visibleModal}
+      >
+        <video
+          controls
+          src='/video.mp4'
+          className='absolute bottom-0 left-0 right-0 top-0 w-[100%]'
+        />
+      </Modal>
+    </>
+  );
+}
