8c9217525b83096fd4e16720be514ffe72abe09b
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-14 09:32:37 +0100

    add spacing in orders page

--- src/app/[locale]/orders/page.tsx
+++ src/app/[locale]/orders/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function OrdersPage() {
   return (
     <div className='container'>
-      <div className='flex w-full py-4 sm:flex-col'>
+      <div className='flex w-full py-12 sm:flex-col'>
         <SidebarFilter />
         <div className='flex w-full flex-wrap justify-between gap-y-5 overflow-y-scroll p-2 sm:mb-6'>
           <OrderCard
