574f956d2de77ddd860d486ee5d0e60db2a732b7
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-16 15:10:23 +0100

    fix sliders

--- src/components/core/sliders/latest-jobs-slider.tsx
+++ src/components/core/sliders/latest-jobs-slider.tsx
@@ -12,17 +12,28 @@ export function LatestJobsSlider() {
     <Slider
       arrows={false}
       className='my-5'
+      slidesToShow={3}
+      slidesToScroll={2}
+      initialSlide={2}
+      autoplay
+      cssEase='linear'
       responsive={[
         {
-          breakpoint: 2600,
+          breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            centerMode: true,
+            slidesToShow: 2.5,
+            slidesToScroll: 1,
           },
         },
         {
-          breakpoint: 767,
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1.5,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
