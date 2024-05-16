5eae4f41902e3fea15d2a6582e79bb13b5d46f38
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-16 14:40:19 +0100

    fixing the download section

--- src/components/core/landing-page/download-app-section.tsx
+++ src/components/core/landing-page/download-app-section.tsx
@@ -9,36 +9,40 @@ export async function DownloadAppSection() {
   return (
     <section className="bg-[url('/download-app-background.svg')] bg-cover bg-no-repeat py-12">
       <div className='container flex items-center sm:flex-col-reverse sm:gap-6'>
-        <div className='relative flex w-[50%] items-center justify-center sm:w-full sm:flex-wrap'>
+        <div className='flex w-[50%] items-center justify-center sm:w-full sm:flex-wrap'>
           <ImageShapeMakerSvg
             screenshot='/screenshots/blanzin.svg'
             id='DownloadApp'
-          />
-          <Link
-            className='absolute right-14 top-[60%] flex w-[150px] items-center gap-1 rounded bg-main p-2 text-xs font-medium text-backgroundDark  sm:-right-3'
-            href='https://play.google.com'
-            target='_blank'
+            className='relative'
           >
-            <Image
-              className='w-[2vw] sm:w-[8vw]'
-              src={require('@/assets/images/icons/apple.svg')}
-              alt=''
-            />
-            <span>{t('links.app-store')}</span>
-          </Link>
-          <Link
-            target='_blank'
-            className='absolute right-14 top-[50%] flex w-[150px] items-center gap-1 rounded bg-main p-2 text-xs font-medium  text-backgroundDark sm:-right-3'
-            href='https://play.google.com'
-          >
-            <Image
-              className='w-[2vw] sm:w-[8vw]'
-              src={require('@/assets/images/icons/play-store.svg')}
-              alt=''
-            />
+            <>
+              <Link
+                className='absolute -right-16 top-[60%] flex w-[150px] items-center gap-1 rounded bg-main p-2 text-xs font-medium text-backgroundDark  sm:-right-6'
+                href='https://play.google.com'
+                target='_blank'
+              >
+                <Image
+                  className='icon'
+                  src={require('@/assets/images/icons/apple.svg')}
+                  alt=''
+                />
+                <span>{t('links.app-store')}</span>
+              </Link>
+              <Link
+                target='_blank'
+                className='absolute -right-16 top-[50%] flex w-[150px] items-center gap-1 rounded bg-main p-2 text-xs font-medium  text-backgroundDark sm:-right-6'
+                href='https://play.google.com'
+              >
+                <Image
+                  className='icon'
+                  src={require('@/assets/images/icons/play-store.svg')}
+                  alt=''
+                />
 
-            <span>{t('links.play-store')}</span>
-          </Link>
+                <span>{t('links.play-store')}</span>
+              </Link>
+            </>
+          </ImageShapeMakerSvg>
         </div>
 
         <div className='flex w-[40%] flex-col items-center gap-12 sm:w-full'>
@@ -52,14 +56,16 @@ export async function DownloadAppSection() {
             width={200}
             height={200}
           />
-          <Button theme='secondary' className='group '>
-            <Link
-              className='text-main group-hover:text-secondary'
-              href='/download-app'
-            >
-              {t('download-app-btn-text')}
-            </Link>
-          </Button>
+          <div className='w-[50%] sm:w-full'>
+            <Button theme='secondary' className='group '>
+              <Link
+                className='text-main group-hover:text-secondary'
+                href='/download-app'
+              >
+                {t('download-app-btn-text')}
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
     </section>
