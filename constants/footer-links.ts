9802e717a160ada9894e6a5a03f6685c1ceee418
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-19 11:36:37 +0100

    fix footer link and fix filter in jobs page

--- constants/footer-links.ts
+++ constants/footer-links.ts
@@ -1,80 +1,78 @@
-import { getI18n } from "@/utils/locales/server";
-
 export const footerLinks: {
   id: number;
   section:
-    | "section.platform"
-    | "section.blanzin"
-    | "section.support"
-    | "section.apps";
+    | 'section.platform'
+    | 'section.blanzin'
+    | 'section.support'
+    | 'section.apps';
   items: {
     id: number;
     icon?: any;
     label:
-      | "links.find-expert"
-      | "links.become-expert"
-      | "links.trainee-in-germany"
-      | "links.jobs-in-germany"
-      | "links.about-us"
-      | "links.privacy-policy"
-      | "links.customer-policy"
-      | "links.blog"
-      | "links.contact-us"
-      | "links.feedback"
-      | "links.app-store"
-      | "links.play-store"
-      | "links.faq";
+      | 'links.find-expert'
+      | 'links.become-expert'
+      | 'links.trainee-in-germany'
+      | 'links.jobs-in-germany'
+      | 'links.about-us'
+      | 'links.privacy-policy'
+      | 'links.customer-policy'
+      | 'links.blog'
+      | 'links.contact-us'
+      | 'links.feedback'
+      | 'links.app-store'
+      | 'links.play-store'
+      | 'links.faq';
     link: string;
   }[];
 }[] = [
   {
     id: 1,
-    section: "section.platform",
+    section: 'section.platform',
     items: [
-      { id: 11, label: "links.find-expert", link: "/find-expert" },
-      { id: 12, label: "links.become-expert", link: "/become-expert" },
+      { id: 11, label: 'links.find-expert', link: '/new-order' },
+      { id: 12, label: 'links.become-expert', link: '/experts' },
       {
         id: 13,
-        label: "links.trainee-in-germany",
-        link: "/trainee-in-germany",
+        label: 'links.trainee-in-germany',
+        link: '/jobs?type=trainee',
       },
-      { id: 14, label: "links.jobs-in-germany", link: "/jobs-in-germany" },
+      { id: 14, label: 'links.jobs-in-germany', link: '/jobs?type=employee' },
     ],
   },
   {
     id: 2,
-    section: "section.blanzin",
+    section: 'section.blanzin',
     items: [
-      { id: 21, label: "links.about-us", link: "/about" },
-      { id: 22, label: "links.blog", link: "/blog" },
-      { id: 23, label: "links.privacy-policy", link: "/privacy-policy" },
-      { id: 24, label: "links.customer-policy", link: "/customer-policy" },
-      { id: 25, label: "links.faq", link: "/faq" },
+      { id: 21, label: 'links.about-us', link: '/about' },
+      { id: 22, label: 'links.blog', link: '/blog' },
+      { id: 23, label: 'links.privacy-policy', link: '/privacy-policy' },
+      { id: 24, label: 'links.customer-policy', link: '/customer-policy' },
+      { id: 25, label: 'links.faq', link: '/faq' },
     ],
   },
   {
     id: 3,
-    section: "section.support",
+    section: 'section.support',
     items: [
-      { id: 11, label: "links.contact-us", link: "/contact" },
-      { id: 12, label: "links.feedback", link: "/feedback" },
+      { id: 11, label: 'links.contact-us', link: '/contact' },
+      { id: 12, label: 'links.feedback', link: '/feedback' },
     ],
   },
   {
     id: 4,
-    section: "section.apps",
+    section: 'section.apps',
     items: [
       {
         id: 1,
-        label: "links.app-store",
-        link: "",
-        icon: require("@/assets/images/icons/apple.svg"),
+        label: 'links.app-store',
+        link: '',
+        icon: require('@/assets/images/icons/apple.svg'),
       },
       {
         id: 2,
-        label: "links.play-store",
-        link: "",
-        icon: require("@/assets/images/icons/play-store.svg"),
+        label: 'links.play-store',
+        link: '',
+        icon: require('@/assets/images/icons/play-store.svg'),
       },
     ],
   },
--- src/components/core/jobs-filter/jobs-filter.tsx
+++ src/components/core/jobs-filter/jobs-filter.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { DropdownMultipleSelect } from '@/components/common';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 type Props = {};
 
 export function JobsFilter({}: Props) {
   const { push } = useRouter();
+  const { control } = useForm();
 
   return (
-    <div className='my-3 flex min-w-[30%] flex-col items-center justify-center rounded-md bg-backgroundSecondary p-2 dark:bg-backgroundSecondaryDark sm:w-full'>
+    <div className='my-3 flex min-w-[30%] flex-col items-center justify-center rounded-md border-[1px] border-border bg-backgroundSecondary p-2 dark:bg-backgroundSecondaryDark sm:w-full'>
       <div className='flex w-full gap-2 rounded-full border-[1px] border-[#CFD6E7] bg-backgroundSecondary p-2 dark:bg-backgroundSecondaryDark sm:w-full'>
         <Image
           alt=''
@@ -22,6 +25,28 @@ export function JobsFilter({}: Props) {
           placeholder='query'
           type='text'
           className='w-full bg-backgroundSecondary text-text caret-[#CFD6E7] focus:border-none focus:outline-none dark:bg-backgroundSecondaryDark dark:text-textdark'
+        />
+      </div>
+      <div className=''>
+        <DropdownMultipleSelect
+          wrapperClassName='mt-3'
+          className=''
+          items={['Casablanca', 'Fes', 'Marrakech', 'Rabat', 'Tanger']}
+          control={control}
+          extractDisplayMember={(item) => item}
+          extractValueMember={(item) => item}
+          label='Location'
+          name='cities'
+        />
+        <DropdownMultipleSelect
+          wrapperClassName='mt-3'
+          className=''
+          items={['Casablanca', 'Fes', 'Marrakech', 'Rabat', 'Tanger']}
+          control={control}
+          extractDisplayMember={(item) => item}
+          extractValueMember={(item) => item}
+          label='Location'
+          name='cities'
         />
       </div>
     </div>
