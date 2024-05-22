8dc617642c022d8250e50e4775bef91763080e94
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-22 17:38:47 +0100

    adding privacy policy page

---
+++ src/app/[locale]/feedback/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FeedbackPage() {
+  return (
+    <div className='my-12'>
+      <div className='container'>FeedbackPage</div>
+    </div>
+  );
+}
--- src/app/[locale]/privacy-policy/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PrivacyPolicy() {
-  return (
-    <div className='my-12'>
-      <div className='container'>Next Page</div>
-    </div>
-  );
-}
