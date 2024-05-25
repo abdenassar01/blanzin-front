c8300ea6898dca91617e409aa0ff7181239a4f85
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-25 14:12:56 +0100

    fix build errors

--- src/app/[locale]/(static)/about/page.tsx
+++ src/app/[locale]/(static)/about/page.tsx
@@ -19,14 +19,14 @@ export default async function AboutUs() {
             Our story
           </h2>
           <p className=''>
-            Blanzin's adventure began with a dream - the dream of making a
+            {`Blanzin's adventure began with a dream - the dream of making a
             difference. With many years of experience in IT and business
             management, we decided to use our knowledge and passion to create a
             service that not only meets people's needs, but also supports their
             dreams. As a young Moroccan team from Germany, we have come together
             to create something unique: A platform that not only provides
             expertise, but also strengthens communities and opens up
-            opportunities.
+            opportunities.`}
           </p>
           <Image
             alt=''
@@ -49,13 +49,13 @@ export default async function AboutUs() {
           />
 
           <p className=''>
-            Born from the vision of two passionate IT engineers and experienced
+            {`Born from the vision of two passionate IT engineers and experienced
             business people, Blanzin is not just a platform - it's a promise.
             Our heart beats for innovation and passion for service, paired with
             the firm belief that technology connects people and improves lives.
             Our mission at Blanzin is to build a bridge between experts in
             Morocco and people worldwide to make help and opportunities
-            accessible.
+            accessible.`}
           </p>
         </div>
         <div className='flex w-[25%] flex-col gap-4 md:w-[48%] sm:w-full'>
@@ -63,13 +63,13 @@ export default async function AboutUs() {
             Our community
           </h2>
           <p className=''>
-            Blanzin is more than just a platform - it's a community of people
+            {`Blanzin is more than just a platform - it's a community of people
             who come together to help and support each other. We believe in the
             power of collaboration and pride ourselves on creating a platform
             where experts can share their knowledge and people have the
             opportunity to achieve their goals. In our diverse and dynamic
             community, people of all backgrounds find a place where they feel
-            welcome and where their needs are taken seriously.
+            welcome and where their needs are taken seriously.`}
             <br />
             <br />
             Welcome to Blanzin - where dreams become reality and opportunities
