ccb97434667541ff099a05d98e6842bfa029f91b
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-27 23:50:57 +0100

    adding [add new profile]

---
+++ src/app/[locale]/profile/new/new-profile-form.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Switch } from '@/components';
+import { useScopedI18n } from '@/utils/locales/client';
+import React from 'react';
+
+type Props = {};
+
+export function NewProfileForm({}: Props) {
+  const t = useScopedI18n('role');
+
+  return (
+    <div className='my-3 flex flex-col gap-5 rounded-xl bg-background p-2 dark:bg-backgroundSecondaryDark'>
+      <div className='flex w-full justify-between'>
+        <div className='font-meduim text-xbase text-text dark:text-textdark'>
+          {t('customer')}
+        </div>
+        <Switch
+          active
+          onActiveChange={() => alert('adding customer profile')}
+        />
+      </div>
+      <div className='flex w-full justify-between'>
+        <div className='font-meduim text-xbase text-text dark:text-textdark'>
+          {t('expert')}
+        </div>
+        <Switch active onActiveChange={() => alert('adding expert profile')} />
+      </div>
+      <div className='flex w-full justify-between'>
+        <div className='font-meduim text-xbase text-text dark:text-textdark'>
+          {t('trainee')}
+        </div>
+        <Switch active onActiveChange={() => alert('adding trainee profile')} />
+      </div>
+      <div className='flex w-full justify-between'>
+        <div className='font-meduim text-xbase text-text dark:text-textdark'>
+          {t('employee')}
+        </div>
+        <Switch
+          active
+          onActiveChange={() => alert('adding employee profile')}
+        />
+      </div>
+    </div>
+  );
+}
