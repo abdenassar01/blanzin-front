f8321b3d7633000ec09368cc94780df2a995be0d
abdenassar01 <abdenassaramimi99@gmail.com>
2024-05-31 19:05:38 +0100

    fix profile sidebar links

--- src/app/[locale]/profile/docs/page.tsx
+++ src/app/[locale]/profile/docs/page.tsx
@@ -2,13 +2,18 @@ import { Heading } from '@/components';
 import { getScopedI18n } from '@/utils/locales/server';
 import React from 'react';
 
-export default async function MyDocumentsPage() {
+export default async function MyDocumentsPage({
+  searchParams: { doc },
+}: {
+  searchParams: { doc: string };
+}) {
   const t = await getScopedI18n('profile');
   return (
     <div className='min-h-[40vw] rounded-xl bg-backgroundSecondary p-4 dark:bg-backgroundDark'>
       <div className='text-xxm'>
         <Heading heading={t('docs')} />
       </div>
+      <div className=''>{doc}</div>
     </div>
   );
 }
--- src/components/core/profile/profile-sidebar.tsx
+++ src/components/core/profile/profile-sidebar.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import { ActiveLink } from '@/components/common/active-link/active-link';
+import { cn } from '@/utils';
 import { useScopedI18n } from '@/utils/locales/client';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function ProfileSidebar() {
   const t = useScopedI18n('profile');
 
+  const docsT = useScopedI18n('application');
+
   const searchParams = useSearchParams();
   const role = searchParams.get('role');
+  const docParam = searchParams.get('doc');
+
+  const [expandsDocs, setExpendsDocs] = useState<boolean>(false);
 
   const pages = [
     {
@@ -38,32 +45,45 @@ export function ProfileSidebar() {
     },
     {
       id: 5,
-      link: '/profile/docs',
-      label: t('docs'),
+      link: '/profile/resume',
+      label: docsT('resume'),
       roles: ['trainee', 'employee'],
     },
     {
       id: 6,
+      link: '/profile/docs',
+      label: t('docs'),
+      items: [
+        { label: docsT('lang-certificate'), doc: 'lang' },
+        { label: docsT('diploma'), doc: 'diploma' },
+        { label: docsT('job-certificates'), doc: 'job-certificate' },
+        { label: docsT('intenship'), doc: 'internship' },
+        { label: docsT('acknowledgement'), doc: 'acknowledgement' },
+      ],
+      roles: ['trainee', 'employee'],
+    },
+    {
+      id: 7,
       link: '/profile/orders',
       label: t('my-orders'),
       roles: ['customer'],
     },
 
     {
-      id: 7,
+      id: 8,
       link: '/chat',
       label: t('chat'),
       roles: ['customer', 'expert'],
       root: true,
     },
     {
-      id: 8,
+      id: 9,
       link: '/profile/new',
       label: t('new-profile'),
       roles: ['trainee', 'employee', 'customer', 'expert'],
     },
     {
-      id: 9,
+      id: 10,
       link: '/new-order',
       label: t('new-order'),
       roles: ['customer'],
@@ -73,10 +93,50 @@ export function ProfileSidebar() {
 
   return (
     <ul className='sm:no-scrollbar mt-[30px] w-full sm:mx-3 sm:flex sm:gap-[24px] sm:overflow-y-scroll'>
-      {pages.map(
-        (page) =>
-          page.roles.includes(role || '') && (
-            <li key={page.id} className=' text-cardText w-full text-base'>
+      {React.Children.toArray(
+        pages.map((page) =>
+          page.roles.includes(role || '') && page.items ? (
+            <div className='flex flex-col items-end justify-end'>
+              <li
+                onClick={() => setExpendsDocs((prev) => !prev)}
+                className='w-full text-base'
+              >
+                <ActiveLink
+                  disableHover
+                  link={page.link}
+                  suffix={page.root ? '' : '?role=' + role + '&doc=lang'}
+                  className='relative block h-full w-full whitespace-nowrap rounded-l-lg p-3 dark:bg-backgroundSecondaryDark sm:rounded-t-lg sm:bg-background sm:px-6 sm:py-5 sm:text-mb-base'
+                  activeClassName='prose-em:block bg-backgroundSecondary board-link-active dark:bg-backgroundDark !text-main border-blue-500 border-l-4 font-normal  sm:border-l-0 sm:border-t-1'
+                >
+                  <em className='absolute -top-6 right-0 hidden h-6 w-6 bg-backgroundSecondary dark:bg-backgroundDark dark:after:!bg-backgroundSecondaryDark sm:hidden' />
+                  <div className='flex'>{page.label}</div>
+                </ActiveLink>
+              </li>
+              <div
+                className={cn(
+                  'grid w-[95%] overflow-hidden transition-all',
+                  expandsDocs ? 'h-auto' : 'h-0'
+                )}
+              >
+                {React.Children.toArray(
+                  page.items.map((doc) => (
+                    <Link
+                      href={'/profile/docs?role=' + role + '&doc=' + doc.doc}
+                      className={cn(
+                        'relative min-h-0 p-3',
+                        docParam === doc.doc
+                          ? 'rounded-l-lg border-l-4 border-main bg-backgroundSecondary text-secondary dark:bg-backgroundDark dark:text-main sm:rounded-t-lg'
+                          : ' board-link-active bg-background dark:bg-backgroundSecondaryDark'
+                      )}
+                    >
+                      <span className='text-xs'>{doc.label}</span>{' '}
+                    </Link>
+                  ))
+                )}
+              </div>
+            </div>
+          ) : (
+            <li className='w-full text-base'>
               <ActiveLink
                 disableHover
                 link={page.link}
@@ -90,6 +150,7 @@ export function ProfileSidebar() {
               </ActiveLink>
             </li>
           )
+        )
       )}
     </ul>
   );
