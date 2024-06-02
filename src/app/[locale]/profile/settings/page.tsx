40f48593577e237c145815f2fe99d83799a7388e
abdenassar01 <abdenassaramimi99@gmail.com>
2024-06-02 12:41:15 +0100

    update settings page

--- src/app/[locale]/profile/settings/page.tsx
+++ src/app/[locale]/profile/settings/page.tsx
@@ -1,18 +1,26 @@
-import { SettingSelectedTab, SettingsSidebar } from '@/components';
+import { Heading, SettingSelectedTab, SettingsSidebar } from '@/components';
+import { getScopedI18n } from '@/utils/locales/server';
 import React from 'react';
 
 type Props = {
   searchParams: { tab: 'security' | 'new-profile' | 'appearance' };
 };
 
-export default function SettingsPage({ searchParams: { tab } }: Props) {
+export default async function SettingsPage({ searchParams: { tab } }: Props) {
+  const t = await getScopedI18n('profile');
+
   return (
-    <div className='flex gap-8'>
-      <div className='w-[30%]'>
-        <SettingsSidebar />
+    <div className=''>
+      <div className='text-xxm'>
+        <Heading heading={t('settings')} />
       </div>
-      <div className='w-full'>
-        <SettingSelectedTab selected={tab} />
+      <div className='mt-6 flex gap-8'>
+        <div className='w-[30%]'>
+          <SettingsSidebar />
+        </div>
+        <div className='w-full'>
+          <SettingSelectedTab selected={tab} />
+        </div>
       </div>
     </div>
   );
