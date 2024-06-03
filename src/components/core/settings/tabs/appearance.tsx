b2b0f773e07ded7bed45705827533838891d5713
abdenassar01 <abdenassaramimi99@gmail.com>
2024-06-03 08:21:19 +0100

    fixing change language

--- src/components/core/settings/tabs/appearance.tsx
+++ src/components/core/settings/tabs/appearance.tsx
@@ -1,28 +1,20 @@
 'use client';
 
 import { Switch } from '@/components';
-import { usePathname } from '@/navigation';
 import { cn } from '@/utils';
-import { useCurrentLocale, useScopedI18n } from '@/utils/locales/client';
+import {
+  useChangeLocale,
+  useCurrentLocale,
+  useI18n,
+} from '@/utils/locales/client';
 import { useTheme } from 'next-themes';
-import { useParams, useRouter } from 'next/navigation';
-import React, { startTransition } from 'react';
+import React from 'react';
 
 export function AppearanceTab() {
-  const t = useScopedI18n('appearance-settings');
+  const t = useI18n();
   const { theme, setTheme } = useTheme();
   const locale = useCurrentLocale();
-
-  const router = useRouter();
-  const pathname = usePathname();
-  const params = useParams();
-
-  function onLangClick(nextLocale: string) {
-    startTransition(() => {
-      //@ts-ignore
-      router.replace({ pathname, params }, { locale: nextLocale });
-    });
-  }
+  const changeLocal = useChangeLocale();
 
   const toggleTheme = () => {
     if (theme === 'dark') {
@@ -33,7 +25,7 @@ export function AppearanceTab() {
   };
 
   const languages: {
-    value: string;
+    value: 'en' | 'fr' | 'ar';
     label: 'english' | 'arabic' | 'frensh';
     icon: any;
   }[] = [
@@ -54,29 +46,33 @@ export function AppearanceTab() {
     },
   ];
 
+  console.log('Locale: ', locale);
+
   return (
     <div>
       <div className='flex w-full justify-between'>
         <div className='font-meduim text-base text-text dark:text-textdark'>
-          {t('theme')}
+          {t('appearance-settings.theme')}
         </div>
         <Switch active={theme === 'dark'} onActiveChange={toggleTheme} />
       </div>
-      <div className='flex justify-between'>
+      <div className='flex items-center justify-between'>
         <div className='font-meduim text-base text-text dark:text-textdark'>
-          {t('langue')}
+          {t('appearance-settings.langue')}
         </div>
         <div className='mt-4 flex gap-2'>
           {React.Children.toArray(
             languages.map((lang) => (
               <div
-                onClick={() => onLangClick(lang.value)}
+                onClick={() => changeLocal(lang.value)}
                 className={cn(
-                  'rounded-xl bg-backgroundSecondary px-4 py-2 text-text dark:bg-backgroundDark dark:text-textdark',
-                  lang.value === locale ? 'bg-main text-secondary' : ''
+                  'cursor-pointer rounded-xl bg-backgroundSecondary px-4 py-2 text-text dark:bg-backgroundDark dark:text-textdark',
+                  lang.value === locale
+                    ? 'bg-main text-secondary dark:bg-main dark:text-secondary'
+                    : ''
                 )}
               >
-                {lang.value}
+                {t(lang.label)}
               </div>
             ))
           )}
