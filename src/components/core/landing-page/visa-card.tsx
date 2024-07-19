d173e655643ef01b416ee9fb283c03e039874c88
abdenassar01 <abdenassaramimi99@gmail.com>
2024-07-19 01:12:29 +0100

    adding employee slider + fixes in cards

--- src/components/core/landing-page/visa-card.tsx
+++ src/components/core/landing-page/visa-card.tsx
@@ -11,22 +11,28 @@ type Props = {
 
 export function VisaCard({ visa: { img, title, description } }: Props) {
   return (
-    <div className='flex w-[32%] flex-col justify-between rounded-xl border border-secondary p-2 dark:border-main sm:w-full'>
-      <Image
-        src={img}
-        width={300}
-        height={200}
-        className='w-full rounded'
-        alt='visa'
-      />
-      <div className='mt-3 text-xl font-bold text-secondary dark:text-main'>
-        {title}
+    <div className='flex h-[580px] flex-col justify-between rounded-xl border border-secondary p-2 dark:border-main sm:w-full'>
+      <div className=''>
+        <Image
+          src={img}
+          width={300}
+          height={200}
+          className='w-full rounded'
+          alt='visa'
+        />
+        <div className='mt-3 text-xl font-bold text-secondary dark:text-main'>
+          {title}
+        </div>
       </div>
-      <div className='my-3'>{description}</div>
-      <div className='w-[50%] sm:w-full'>
-        <Link href={`/visa/${title.toLowerCase().replace(/ /g, '-')}`}>
-          <Button text='read more' className='rounded-md' theme='secondary' />
-        </Link>
+      <div className=''>
+        <div className='my-3 text-mainText dark:text-textdark'>
+          {description}
+        </div>
+        <div className='w-[50%] sm:w-full'>
+          <Link href={`/visa/${title.toLowerCase().replace(/ /g, '-')}`}>
+            <Button text='read more' className='rounded-md' theme='secondary' />
+          </Link>
+        </div>
       </div>
     </div>
   );
